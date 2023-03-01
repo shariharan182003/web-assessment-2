fa150b4de38cdf6b5378f4a9b28a702113d1736a
hari <shariharan182003@gmail.com>
2023-03-01 22:24:26 +0530

    q5 alter into a folder

--- 4/4.js
+++ 4/4.js
@@ -7,18 +7,19 @@ function validate(){
     let temp_add=document.getElementById("temp_add").value;
     let per_add=document.getElementById("per_add").value;
     let phone=document.getElementById("phone").value;
-    if(regno(regno)){
+    if(name_check(name)){
         document.write("true")
-    }
-    else{
-        document.write("false")
     }
 }
 function phone_no(phone){
-    const pattern_regex = /^\d{10}$/;
+    const pattern_regex=/^\d{10}$/;
     return pattern_regex.test(phone);
 }
-function regno(regno){
-    const pattern_regex=/^[0-9a-zA-Z]*$/;
+function reg_no(regno){
+    const pattern_regex=/^[a-zA-Z0-9]{9}$/
     return pattern_regex.test(regno);
+}
+function name_check(name){
+    const pattern_regex=/^[a-zA-Z]{1,30}$/
+    return pattern_regex.test(name);
 }
