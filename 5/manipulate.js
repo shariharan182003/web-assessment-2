11c881381558e96ea368c4b272a795cfe506f9f4
Hariharan <hariharanjayachandran04@gmail.com>
2023-03-03 20:10:04 +0530

    5th added

---
+++ 5/manipulate.js
@@ -0,0 +1,29 @@
+let count = 0;
+
+function add() {
+    var data = $("#txtbox").val();
+    $("#list").append("<li>" + data + "</li>");
+
+    $("li:even").css('background-color', 'yellow');
+    $("li:odd").css('background-color', 'white');
+    $("#txtbox").val("");
+}
+
+function remove() {
+    var data = $("#txtbox").val().toLowerCase();
+    var listData = $("#list li");
+    for (let index = 0; index < listData.length; index++) {
+        var lidata = $(listData[index]).text().toLowerCase();
+        if (data==lidata) {
+            $(listData[index]).remove();
+            break;
+        }   
+    }
+    $("li:even").css('background-color', 'yellow');
+    $("li:odd").css('background-color', 'white');
+    $("#txtbox").val("");
+}
+
+// document.readyState(function () {
+//     $("li:even").css('background-color', 'yellow');
+// })
